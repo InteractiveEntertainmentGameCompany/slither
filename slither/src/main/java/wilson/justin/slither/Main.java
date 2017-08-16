09223211da8729104f296b60eeac5fb874e0ca3c
Justin Wilson <juddybeckham@gmail.com>
2017-08-16 04:01:28 -0400

    Published project to Github
    project uploaded to github to start working on it.

---
+++ slither/src/main/java/wilson/justin/slither/Main.java
@@ -0,0 +1,13 @@
+package wilson.justin.slither;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
