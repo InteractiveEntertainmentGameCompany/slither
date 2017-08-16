2df9c1711923e7ae851f19a41db7598cbd680d20
Justin Wilson <juddybeckham@gmail.com>
2017-08-16 08:12:06 -0400

    Integration for Constant/Player
    Configured screen resolution as well as Constant class integration

--- slither/src/main/java/wilson/justin/slither/Main.java
+++ slither/src/main/java/wilson/justin/slither/Main.java
@@ -1,13 +1,25 @@
 package wilson.justin.slither;
 
+import wilson.justin.slither.entity.player.Player;
+
+import com.b3dgs.lionengine.Config;
+import com.b3dgs.lionengine.Version;
+import com.b3dgs.lionengine.core.awt.EngineAwt;
+import com.b3dgs.lionengine.core.sequence.Loader;
+
 /**
- * Hello world!
+ * 
+ * @author Justin
  *
  */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+	
+    public static void main( String[] args ) {
+		Player player = new Player();
+    	EngineAwt.start(Constant.NAME, Version.create(Constant.VERSION[0], Constant.VERSION[1], Constant.VERSION[2]), Main.class);
+    	System.out.println("Game Engine Started...");
+    	Loader.start(Config.windowed(player.getResolution()), Scene.class);
+    	System.out.println("Loading Scene...");
+        
     }
 }
---
+++ slither/src/main/java/wilson/justin/slither/Scene.java
@@ -0,0 +1,35 @@
+package wilson.justin.slither;
+
+import wilson.justin.slither.entity.player.Player;
+
+import com.b3dgs.lionengine.Context;
+import com.b3dgs.lionengine.Resolution;
+import com.b3dgs.lionengine.core.sequence.Sequence;
+import com.b3dgs.lionengine.graphic.Graphic;
+
+public class Scene extends Sequence {
+	
+	private static Player player = new Player();
+    public final static Resolution resolution = player.getResolution();
+
+    public Scene(Context context)
+    {
+        super(context, resolution);
+    }
+
+    @Override
+    public void load()
+    {
+    	// load game
+    }
+
+    public void update(double extrp)
+    {
+        // Update game
+    }
+
+    public void render(Graphic g)
+    {
+        // Render game
+    }
+}
