2477e42689d1777a5b93c4eaac1cc83bb1e0f6d5
Justin Wilson <juddybeckham@gmail.com>
2017-08-16 08:09:31 -0400

    Added Game Constant Class
    Holds all the game constants in one easy to access place.

---
+++ slither/src/main/java/wilson/justin/slither/Constant.java
@@ -0,0 +1,15 @@
+package wilson.justin.slither;
+
+/**
+ * 
+ * @author Justin
+ *
+ */
+public class Constant {
+
+	// The name of the game. 
+	public static final String NAME = "Slitherer";
+	
+	// Version of the game (Major, Minor, Micro)
+	public static final int[] VERSION = {1, 0, 0};
+}
