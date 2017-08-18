a26c3e5a4f36a14d6544c7e09845181265e39de3
Justin Wilson <juddybeckham@gmail.com>
2017-08-18 04:26:08 -0400

    Added Combatant Interface
    Will be used for when an entity hits another entity.

---
+++ slither/src/main/java/wilson/justin/slither/content/combat/Combatant.java
@@ -0,0 +1,14 @@
+package wilson.justin.slither.content.combat;
+
+import wilson.justin.slither.entity.Entity;
+
+/**
+ * 
+ * @author Phantasye, Justin
+ *
+ */
+public interface Combatant {
+
+	String hit(Entity target);
+	
+}
