d6352b93d8bf2b65f76332743c90def88017db13
Justin Wilson <juddybeckham@gmail.com>
2017-08-18 15:17:57 -0400

    Added more constants
    Native resolution used for when the game loads up (Eventually for the login menu screen)

--- slither/src/main/java/wilson/justin/slither/Constant.java
+++ slither/src/main/java/wilson/justin/slither/Constant.java
@@ -1,5 +1,8 @@
 package wilson.justin.slither;
 
+import com.b3dgs.lionengine.LionEngineException;
+import com.b3dgs.lionengine.Resolution;
+
 /**
  * 
  * @author Justin
@@ -12,4 +15,11 @@ public class Constant {
 	
 	// Version of the game (Major, Minor, Micro)
 	public static final int[] VERSION = {1, 0, 0};
+	
+	public static final Resolution NATIVE = new Resolution(320, 240, 60);
+	
+	// Private Constructor
+    private Constant() {
+        throw new LionEngineException(LionEngineException.ERROR_PRIVATE_CONSTRUCTOR);
+    }
 }
